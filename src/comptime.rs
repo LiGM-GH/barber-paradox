b3432164f1115516822c3ddfe1c2ce45ce08e4ce
LiGM-GH <lipkin.grisha@mail.ru>
2025-11-23 15:13:43 +0300

    Updated comptime.rs

--- src/comptime.rs
+++ src/comptime.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code, unused)]
+
 pub struct ShavesSelf<T>(pub T)
 where
     Barber: Shaves<T>,
@@ -10,8 +12,8 @@ where
 
 pub struct Barber;
 
-trait HasShave {}
-trait HasNoShave {}
+pub trait HasShave {}
+pub trait HasNoShave {}
 
 impl HasShave for CanShave {}
 impl HasNoShave for CanNotShave {}
@@ -19,12 +21,13 @@ impl HasNoShave for CanNotShave {}
 pub struct CanShave;
 pub struct CanNotShave;
 
-pub trait ShaveAbility {}
+/// Seal trait: only CanShave and CanNotShave must implement it.
+trait ShaveAbility {}
 
 impl ShaveAbility for CanShave {}
 impl ShaveAbility for CanNotShave {}
 
-trait Shaves<T> {
+pub trait Shaves<T> {
     type Shave: ShaveAbility;
 }
 
@@ -33,7 +36,8 @@ where
     Barber: Shaves<T>,
     <Barber as Shaves<T>>::Shave: HasNoShave,
 {
-    type Shave = CanNotShave;
+    // T wouldn't be here if Barber CanShave T
+    type Shave = <Barber as Shaves<T>>::Shave;
 }
 
 impl<T> Shaves<NeverShavesSelf<T>> for Barber
@@ -41,7 +45,8 @@ where
     Barber: Shaves<T>,
     <Barber as Shaves<T>>::Shave: HasShave,
 {
-    type Shave = CanShave;
+    // T wouldn't be here if Barber CanNotShave T
+    type Shave = <Barber as Shaves<T>>::Shave;
 }
 
 impl Shaves<Barber> for Barber {
@@ -50,22 +55,49 @@ impl Shaves<Barber> for Barber {
 
 type BarberShaves<C> = <Barber as Shaves<C>>::Shave;
 
-/// Barber can't shave a Barber that ShavesSelf
-///
-/// For any T:
-///     ShavesSelf<T> can't exist in the same program that NeverShaves<T>.
-/// Why?
-/// `ShavesSelf<T>`      => `T` has `<Barber: Shaves<T>>::Shave` equal to `CanNotShave`
-/// `NeverShavesSelf<T>` => `T` has `<Barber: Shaves<T>>::Shave` equal to `CanShave`
-/// which can't be simultaneously true.
-const SHAVES_BARBER: BarberShaves<ShavesSelf<Barber>> = CanNotShave;
+// /// Barber can't shave a Barber that ShavesSelf
+// ///
+// /// For any T:
+// ///     ShavesSelf<T> can't exist in the same program that NeverShaves<T>.
+// ///
+// /// Why?
+// /// `ShavesSelf<T>`      => `T` has `<Barber: Shaves<T>>::Shave` equal to `CanNotShave`
+// /// `NeverShavesSelf<T>` => `T` has `<Barber: Shaves<T>>::Shave` equal to `CanShave`
+// /// which can't be simultaneously true.
+// const SHAVES_BARBER: BarberShaves<ShavesSelf<Barber>> = CanNotShave;
 
-/// Barber can't shave a Barber that NeverShavesSelf
-///
-/// For any T:
-///     ShavesSelf<T> can't exist in the same program that NeverShaves<T>.
-/// Why?
-/// `ShavesSelf<T>`      => `T` has `<Barber: Shaves<T>>::Shave` equal to `CanNotShave`
-/// `NeverShavesSelf<T>` => `T` has `<Barber: Shaves<T>>::Shave` equal to `CanShave`
-/// which can't be simultaneously true.
-const NEVER_SHAVES_BARBER: BarberShaves<NeverShavesSelf<Barber>> = CanShave;
+// /// Barber can't shave a Barber that NeverShavesSelf
+// ///
+// /// For any T:
+// ///     ShavesSelf<T> can't exist in the same program that NeverShaves<T>.
+// ///
+// /// Why?
+// /// `ShavesSelf<T>`      => `T` has `<Barber: Shaves<T>>::Shave` equal to `CanNotShave`
+// /// `NeverShavesSelf<T>` => `T` has `<Barber: Shaves<T>>::Shave` equal to `CanShave`
+// /// which can't be simultaneously true.
+// const NEVER_SHAVES_BARBER: BarberShaves<NeverShavesSelf<Barber>> = CanShave;
+
+// Ok, if Barber CanNotShave Barber, then Barber ShavesSelf
+//
+// Now we know that Barber ShavesSelf, that implies that Barber CanNotShave Barber.
+//
+// That implies that ShavesSelf<ShavesSelf<Barber>> must not exist as Barber
+const CHECK_NON_SHAVES: BarberShaves<ShavesSelf<ShavesSelf<Barber>>> = CanNotShave;
+// const CHECK_AGAIN: BarberShaves<NeverShavesSelf<ShavesSelf<Barber>>> = CanNotShave;
+
+// const CHECK_SHAVES: BarberShaves<NeverShavesSelf<Barber>> = CanShave;
+
+// const SHAVING_BARBER: ShavesSelf<Barber> = ShavesSelf(Barber);
+// const NONSHAVING_BARBER: NeverShavesSelf<Barber> = NeverShavesSelf(Barber);
+
+#[cfg(test)]
+mod tests {
+    use crate::comptime::{Barber, NeverShavesSelf, ShavesSelf};
+
+    #[test]
+    fn test_shaves() {
+        // Here Barber must Shave<Barber>, and that's done.
+        // and since there's a blanket implementation that Barber must Shave any ShavesSelf,
+        // but barber can't since some Barber already
+    }
+}
